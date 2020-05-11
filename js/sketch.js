7bf08f91e3cbb25a44dc26c14be3cd41e11182c2
EmilHHTX <49984359+EmilHHTX@users.noreply.github.com>
2020-05-11 08:14:00 +0200

    j

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,29 @@
-function test() {
 
+
+class Spil {
+    constructor(text, navn) {
+        this.text = text;
+        this.navn = navn;
+    }
 }
 
-function setup() {
-    var can = createCanvas(400, 400);
+let x = 0
 
+function setup() {
+    let can = createCanvas(400, 400);
     can.parent('canvas-container')
+    let width = can.width;
 }
 
 function draw() {
-    background(0);
+    background(0)
+    circle(x, 30, 30);
+
+
+    x += 3;
+    if (x > width) {
+        x = 0;
+    }
+
 
 }
