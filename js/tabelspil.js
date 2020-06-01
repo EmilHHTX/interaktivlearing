220c3069aeebf62cb25fcf2ced69778c43de2db7
BusterDHTX <49984399+BusterLD@users.noreply.github.com>
2020-06-01 16:55:31 +0200

    Update tabelspil.js
    sup dude :))

--- js/tabelspil.js
+++ js/tabelspil.js
@@ -1,149 +1,184 @@
+// Vi starter med at lave globale variabler i programmet så de kan bruges i alle de forskellige klasser, funktioner osv.
 let screenwidth = 700;
 let screenheight = 700;
 let objects = [];
-let ettabel = [1,2,3,4,5,6,7,8,98,09]
+let img;
+// Da vi skal arbejde med tabeller i dette program laver vi "en liste med lister i sig" som inde holder alle de 10 grundtabeller.
 var tabeller = []
-for (let y = 1; y <= 10; y++) 
-  {
+for (let y = 1; y <= 10; y++) {
   var temptabel = []
-  for (let x = 1; x <= 10; x++) 
-    {
-    temptabel.push(y*x);
-    }
-  tabeller.push(temptabel);
+  for (let x = 1; x <= 10; x++) {
+    temptabel.push(y * x);
   }
-
-console.log(tabeller)
+  tabeller.push(temptabel);
+}
 let tabelnumber = 0
+var whichTabel = Math.floor(Math.random() * 10);
 
-class Falling
-{
- constructor(i, correctAnswer, nummer)
-  {
-    this.spawnx = 100 + (i*100);
+// Denne klasse er de objekter som falder ned fra toppen af skærmen.
+class Falling {
+  // I constructoren til denne klasse bliver der modtaget 3 værdier, i og correct answer er to værdier der hjælper os med at finde ud af hvilket af de faldende objekter skal være det korrekte svar, og nummer som er den værdi der fortæller hvor langt spilleren er kommet i tabellen. 
+  constructor(i, correctAnswer, nummer) {
+    this.spawnx = 100 + (i * 150);
     this.y = 1;
     this.speed = 5;
     this.diameter = 10;
-    this.color= color(255,255,255)
+    this.color = color(255, 255, 255)
     this.correct = false
-    this.tal = 0
-    
-    if(i == correctAnswer)
-    {
+    this.tal = Math.floor(Math.random() * 100);
+
+    if (this.tal == tabeller[whichTabel][nummer]) {
+      this.tal = Math.floor(Math.random() * 100);
+    }
+
+
+    if (i == correctAnswer) {
       this.color = color(255, 0, 0)
       this.correct = true
-      this.tal = tabeller[0][nummer]
-      
+      this.tal = tabeller[whichTabel][nummer]
+
     }
+
+
   }
-  
-  drawObjects()
-  { 
+  // Her er vores draw command for de faldene tal, på skærmen kan man kun se tallene falde ned, men bag tallene har vi lagt en cirkel som ikke bliver vist på skærmen på grund af funktionerne noStroke og noFill. Cirklerne bag teksten er tallenes "hitbox"
+  draw() {
     stroke(5)
-    noFill()
+    fill(color(0))
     textSize(32)
-    text(this.tal, this.spawnx-8, this.y+10)    
-    
+    textStyle(NORMAL);
+    text(this.tal, this.spawnx - 8, this.y + 10)
+
     noStroke()
     noFill()
     circle(this.spawnx, this.y, this.diameter)
   }
-  
-  move()
-  {
+
+  // Denne metode er meget simpel, den ændre blot de faldende objekters position med den givne hastighed af objektet
+  move() {
     this.y += this.speed;
   }
-  
-  kollision(other)
-  {
+
+  // Denne metode tjekker om de faldende objekter støder ind i et andet objekt "other" i dette tilfælde er det spilleren.
+  kollision(other) {
     var d = dist(this.spawnx, this.y, other.x, other.y);
-    
-    if (d < (this.diameter/2) + (other.size/2)) 
-    {
+
+    if (d < (this.diameter / 2) + (other.size / 2)) {
       return true;
     }
-      
-    else
-    {
+
+    else {
       return false;
     }
   }
-  
+
 }
 
-class Player
-{
-  constructor()
-  {
-    this.x = screenwidth/2
+// Denne klasse skal skabe objektet som spilleren kan styre.
+class Player {
+  constructor() {
+    this.x = screenwidth / 2
     this.y = screenheight
-    this.size = 15
+    this.size = 100
     this.speed = 5
   }
-  
-  move()
-  {
-    if(keyIsDown(LEFT_ARROW))
-    {
+
+  // Denne metode flytter spilleren højre eller venstre alt efter hvilken en af piletasterne bliver trykket ned.
+  move() {
+    if (keyIsDown(LEFT_ARROW)) {
       this.x -= 5
     }
-    
-    if(keyIsDown(RIGHT_ARROW))
-    {
+
+    if (keyIsDown(RIGHT_ARROW)) {
       this.x += 5
     }
   }
-  
-  drawPlayer()
-  {
+
+  // I draw commanden tegner vi spilleren, vi har valgt at indsætte et billede til spilleren, dog har vi igen tegnet en "usynlig" cirkel som følger positionen af spilleren, vi bruger en cirkel da det simplificerer collision-detection processen 
+  draw() {
     noFill()
-    stroke(5)
-    circle(this.x, this.y-this.size-10, this.size)
+    noStroke()
+    circle(this.x + 3, this.y - 80, this.size);
+    image(img, this.x - 55, this.y - 150, 125, 150);
+
+
   }
-  
+
 }
 
-function createFalling(nummer)
-{
+// I funktionen preload får vi programmet til at indlæse billedfilerne der skal bruges som det første for at optimerer programmet mest muligt.
+function preload() {
+  img = loadImage('progeksamentingninjafærdigcropped.png');
+  backimg = loadImage('japanese-landscape-drawing-65.jpg');
+}
+
+// Denne funktion bruger vi til at skabe en ny liste af objekter fra "Falling" classen, denne funktion er meget vigtig da den bliver udført hver gang objekterne rammer bunden af skærmen.
+function createFalling(nummer) {
   let numberOfFalling = 4
   objects = [];
   let cA = Math.floor(Math.random() * 4);
-
-  for (let i = 0; i<numberOfFalling ; i++)
-  {
-    objects.push (new Falling(i, cA, nummer))
+  for (let i = 0; i < numberOfFalling; i++) {
+    objects.push(new Falling(i, cA, nummer))
   }
-  
-  
 }
 
-function setup() 
-{
+// Functionen setupspil gør det muligt for os at have en reset knap der genstarter spillet med en ny tabel osv.
+function setupspil() {
   createCanvas(screenwidth, screenheight);
-
-  createFalling(tabelnumber)  
+  createFalling(tabelnumber)
   player = new Player()
+  whichTabel = Math.floor(Math.random() * 10);
+  tabelnumber = 0
+
 }
 
+
+// setup funktionen er essentiel i p5 det er en function der kun kører en enkelt gang efter der bliver trykket på p5's startknap. I denne funktion bruger vi setupspil-funktionen og opsætter en reset-knap.
+function setup() {
+  setupspil()
+  button = createButton('Reset');
+  button.mousePressed(setupspil);
+}
+
+// Draw commanden er igen en basis funktion der bliver kørt flere gange i sekundet. 
 function draw() {
-  background(220);
-  player.draw();  
+  background(255, 255, 255);
+  image(backimg, 0, 0, screenwidth, screenheight);
+  player.draw();
   player.move();
-  
-  for (let j = 0; j<4 ; j++)
-  {
-    objects[j].draw();
-    objects[j].move();
-    if (objects[j].kollision(player) == true && objects[j].correct == true)
-    {    
-      tabelnumber += 1
-      createFalling(tabelnumber);
-    }
-  
-    if ( objects[j].y > screenheight)
-    {
-      createFalling(tabelnumber);
+
+  // Vi har delt draw op i to dele med et if-else statement, den første del er hvis man er igang med at spille en tabel.
+  if (tabelnumber < 10) {
+    fill(color(0))
+    textSize(32)
+    textStyle(BOLD);
+    text("Du spiller " + (whichTabel + 1) + " tabellen", screenwidth / 4, 100)
+
+
+    // Her bruger vi også forloops for at sikre os alle objekternes metoder bliver udført.   
+    for (let j = 0; j < 4; j++) {
+      objects[j].draw();
+      objects[j].move();
+      // Her bruger vi endnu et if-statement til at tjekke om spilleren kolliderer med det korrekte tal i tabellen. 
+      if (objects[j].kollision(player) == true && objects[j].correct == true) {
+        tabelnumber += 1
+        createFalling(tabelnumber);
+      }
+      // Som nævnt tidligere i programmet bruger vi funktionen createFalling til at skabe en helt ny liste af objekter når de faldende objekter rammer bunden af skærmen.
+      if (objects[j].y > screenheight) {
+        createFalling(tabelnumber);
+      }
     }
   }
-
+  // Den anden del af draw commanden er når du har færdiggjort en tabel, i denne del stopper spillet og der kommer en tekst op på skræmen der beder brugeren om at trykke på reset-knappen hvis de vil spille igen. 
+  else {
+    textSize(30)
+    fill(color(0))
+    textStyle(BOLD);
+    text("DU HAR KLARET TABELLEN GODT ARBEJDE!", screenwidth / 200, 50)
+    textSize(20);
+    fill(color(0))
+    textStyle(BOLD);
+    text("Hvis du vil spille en ny tabel kan du trykke på 'Reset' knappen", screenwidth / 200, 80)
+  }
 }
